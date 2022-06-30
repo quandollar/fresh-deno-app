cc39b7d891d9afb4d52645ac53471f368e81eab6
Quan Nguyen <quanwnn@gmail.com>
2022-06-30 15:45:06 -0400

    change index page's message

--- routes/index.tsx
+++ routes/index.tsx
@@ -12,8 +12,7 @@ export default function Home() {
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
       <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
+        Simple web app made with Fresh. Click on the + and - buttons to increment and decrement the counter below.
       </p>
       <Counter start={3} />
     </div>
